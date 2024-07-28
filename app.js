4d4c183fcf83926db711ec3bf1c8c1e8e6c195d7
austin-area61 <austinonyango.area61@gmail.com>
2024-07-28 08:57:39 +0300

    added the function that fetches the weather status from the OPEWEATHER API.

--- app.js
+++ app.js
@@ -2,3 +2,16 @@ document.getElementById('search-btn').addEventListener('click', function() {
   const city = document.getElementById('search-input').value;
   fetchWeatherData(city);
 });
+function fetchWeatherData(city) {
+  const apiKey = '5ea84661f674d77fcd0e68b11d6a7464'; //API KEY
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  
+  fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+      displayWeatherData(data);
+  })
+  .catch(error => {
+      console.error('Error fetching weather data:', error);
+  });
+}
