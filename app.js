7f6a5a710eb942dfea8282cb9946544572732e81
austin-area61 <austinonyango.area61@gmail.com>
2024-07-28 09:05:50 +0300

    defined a function that takes data as a argument, extract data from API response then updates the HTML elements

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.getElementById('search-btn').addEventListener('click', function() {
 function fetchWeatherData(city) {
   const apiKey = '5ea84661f674d77fcd0e68b11d6a7464'; //API KEY
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-  
+
   fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
@@ -14,4 +14,20 @@ function fetchWeatherData(city) {
   .catch(error => {
       console.error('Error fetching weather data:', error);
   });
+}
+
+function displayWeatherData(data) {
+  const city = data.name;
+  const country = data.sys.country;
+  const temperature = data.main.temp;
+  const condition = data.weather[0].description;
+  const icon = data.weather[0].icon;
+  const date = new date().toLocaleDateString();
+
+      document.getElementById('city').textContent = `${city}, ${country}`;
+    document.getElementById('date').textContent = date;
+    document.getElementById('temperature').textContent = `${temperature} °C`;
+    document.getElementById('condition').textContent = condition.charAt(0).toUpperCase() + condition.slice(1);
+    document.getElementById('weather-icon').src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+    document.getElementById('weather-icon').alt = condition;
 }
